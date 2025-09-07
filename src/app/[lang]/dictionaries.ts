f26040e3c65c0b8bd61424f6e5f17721904f7572
derekcantillo <cantilloderek@gmail.com>
2025-09-07 08:23:05 -0500

    revert: move dictionaries back to messages

--- src/app/[lang]/dictionaries.ts
+++ src/app/[lang]/dictionaries.ts
@@ -1,10 +1,8 @@
 import 'server-only';
 
 const dictionaries = {
-  en: () =>
-    import('../../../dictionaries/en.json').then(module => module.default),
-  es: () =>
-    import('../../../dictionaries/es.json').then(module => module.default),
+  en: () => import('../../../messages/en.json').then(module => module.default),
+  es: () => import('../../../messages/es.json').then(module => module.default),
 };
 
 export const getDictionary = async (locale: 'en' | 'es') =>
