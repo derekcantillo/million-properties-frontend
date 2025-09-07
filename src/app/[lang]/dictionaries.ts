c96dfbe7a3b4ab63fe1aab153fa94c1a32d3261e
derekcantillo <cantilloderek@gmail.com>
2025-09-07 08:10:04 -0500

    feat: add English and Spanish dictionaries for internationalization

--- src/app/[lang]/dictionaries.ts
+++ src/app/[lang]/dictionaries.ts
@@ -1,8 +1,10 @@
 import 'server-only';
 
 const dictionaries = {
-  en: () => import('../../../messages/en.json').then(module => module.default),
-  es: () => import('../../../messages/es.json').then(module => module.default),
+  en: () =>
+    import('../../../dictionaries/en.json').then(module => module.default),
+  es: () =>
+    import('../../../dictionaries/es.json').then(module => module.default),
 };
 
 export const getDictionary = async (locale: 'en' | 'es') =>
