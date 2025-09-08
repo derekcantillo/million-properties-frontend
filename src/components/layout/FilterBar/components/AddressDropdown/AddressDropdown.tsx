44eba3794e70218933478e91fab7bbb5aff9aa9a
derekcantillo <cantilloderek@gmail.com>
2025-09-07 19:15:03 -0500

    feat: update AddressDropdown layout and improve suggested areas display

--- src/components/layout/FilterBar/components/AddressDropdown/AddressDropdown.tsx
+++ src/components/layout/FilterBar/components/AddressDropdown/AddressDropdown.tsx
@@ -44,47 +44,45 @@ export const AddressDropdown: React.FC<BaseDropdownProps> = ({
 				maxWidth: '100%'
 			}}
 		>
-			<div className="p-4">
-				<div className="space-y-3">
-					<Typography
-						variant={TypographyVariant.SMALL}
-						className="font-medium"
-						fontFamily={TypographyFontFamily.CAIRO}
-					>
-						Buscar por dirección
-					</Typography>
-					<div className="flex min-h-[120px] items-center justify-center gap-4 text-gray-500">
-						<div className="flex flex-3/5 flex-wrap gap-2">
-							{suggestedAreas.map(area => (
-								<button
-									type="button"
-									key={area}
-									className={clsx(
-										'flex cursor-pointer items-center gap-2',
-										'rounded-md border border-gray-200 p-2 hover:bg-gray-200',
-										{
-											'bg-black': area === 'All'
-										}
-									)}
+			<div className="flex flex-col gap-4 p-4">
+				<Typography
+					variant={TypographyVariant.SMALL}
+					className="font-medium"
+					fontFamily={TypographyFontFamily.CAIRO}
+				>
+					Areas sugeridas
+				</Typography>
+				<div className="flex items-center justify-center gap-4 text-gray-500">
+					<div className="flex flex-3/5 flex-wrap gap-2">
+						{suggestedAreas.map(area => (
+							<button
+								type="button"
+								key={area}
+								className={clsx(
+									'flex cursor-pointer items-center gap-2',
+									'rounded-md border border-gray-200 p-2 hover:bg-gray-200',
+									{
+										'bg-black': area === 'All'
+									}
+								)}
+							>
+								<Typography
+									variant={TypographyVariant.SMALL}
+									className="font-medium"
+									fontFamily={TypographyFontFamily.CAIRO}
+									textColor={
+										area === 'All'
+											? TypographyTextColor.WHITE
+											: TypographyTextColor.DEFAULT
+									}
 								>
-									<Typography
-										variant={TypographyVariant.SMALL}
-										className="font-medium"
-										fontFamily={TypographyFontFamily.CAIRO}
-										textColor={
-											area === 'All'
-												? TypographyTextColor.WHITE
-												: TypographyTextColor.DEFAULT
-										}
-									>
-										{area}
-									</Typography>
-								</button>
-							))}
-						</div>
-						<div className="flex h-full flex-2/5 items-center justify-center border">
-							<Typography>Map area</Typography>
-						</div>
+									{area}
+								</Typography>
+							</button>
+						))}
+					</div>
+					<div className="flex h-60 flex-2/5 items-center justify-center border">
+						<Typography>Map area</Typography>
 					</div>
 				</div>
 			</div>
