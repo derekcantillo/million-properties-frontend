74afbbd858bb424521c3035dca646f30199fed79
derekcantillo <cantilloderek@gmail.com>
2025-09-09 08:24:23 -0500

    feat: add typography elements to DynamicHeaderFilter for enhanced visual appeal

--- src/components/layout/DynamicHeaderFilter/DynamicHeaderFilter.tsx
+++ src/components/layout/DynamicHeaderFilter/DynamicHeaderFilter.tsx
@@ -2,6 +2,13 @@
 import React, { useState } from 'react'
 
 import { FilterBar, Header } from '@/components/layout'
+import {
+	Typography,
+	TypographyFontFamily,
+	TypographyTextColor,
+	TypographyVariant,
+	TypographyWeight
+} from '@/components/ui'
 
 export const DynamicHeaderFilter = () => {
 	const [showBackdrop, setShowBackdrop] = useState(false)
@@ -12,6 +19,26 @@ export const DynamicHeaderFilter = () => {
 				<Header />
 				<div className="absolute inset-0 h-full w-full">
 					<div className="absolute inset-0 bg-black/50" />
+					<div className="absolute inset-0 flex flex-col items-center justify-center">
+						<Typography
+							variant={TypographyVariant.H2}
+							fontFamily={TypographyFontFamily.CINZEL}
+							weight={TypographyWeight.NORMAL}
+							className="transition-all duration-300 ease-in-out"
+							textColor={TypographyTextColor.WHITE}
+						>
+							#1 TEAM IN THE US
+						</Typography>
+						<Typography
+							variant={TypographyVariant.H3}
+							fontFamily={TypographyFontFamily.CINZEL}
+							weight={TypographyWeight.NORMAL}
+							className="transition-all duration-300 ease-in-out"
+							textColor={TypographyTextColor.WHITE}
+						>
+							More Than $2.1 Billion
+						</Typography>
+					</div>
 					<video
 						src="assets/videos/hero.mp4"
 						autoPlay
